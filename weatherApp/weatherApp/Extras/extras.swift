889aa0e43748c74f6de3eac5a43b63d6f589557f
SourabhK <101521492@student.swin.edu.au>
2019-06-02 05:29:10 +1000

    Update extras.swift

--- weatherApp/weatherApp/Extras/extras.swift
+++ weatherApp/weatherApp/Extras/extras.swift
@@ -10,11 +10,11 @@ import Foundation
 
 var location = Location()
 
-let currentWeather_API_URL="https://api.openweathermap.org/data/2.5/weather?lat=-37.81&lon=144.96&appid=b1c7bd83b1ca43dc4ffea507d5aea544"
+let currentWeather_API_URL="https://api.openweathermap.org/data/2.5/weather?lat=-37.81&lon=144.96&appid=******************"
 
-//let weatherForecast_API_URL="https://api.openweathermap.org/data/2.5/forecast/daily?lat=-37.814&lon=144.96332&cnt=10&appid=b1c7bd83b1ca43dc4ffea507d5aea544"
+//let weatherForecast_API_URL="https://api.openweathermap.org/data/2.5/forecast/daily?lat=-37.814&lon=144.96332&cnt=10&appid=********************"
 
-var weatherForecast_API_URL="https://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=\(Location.sharedInstance.forecastDays)&appid=b1c7bd83b1ca43dc4ffea507d5aea544"
+var weatherForecast_API_URL="https://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=\(Location.sharedInstance.forecastDays)&appid=********************"
 
 
 
